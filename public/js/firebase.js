ff9431568c095f6097e812937a4bc4af145d2814
pablo179 <castillo.morales.pablo.prog@gmail.com>
2019-06-30 00:29:24 -0500

    about us almost complete

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBSWxEC-Mq0g-VSRLMJSQhSyG4yZ_t2rE8",
+  authDomain: "energysolutionsmengar.firebaseapp.com",
+  databaseURL: "https://energysolutionsmengar.firebaseio.com",
+  projectId: "energysolutionsmengar",
+  storageBucket: "",
+  messagingSenderId: "617730690775",
+  appId: "1:617730690775:web:bfc15a49cb21ab30"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
